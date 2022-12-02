ffbb28e952cb8bbac2bd4d4200e1a801ef106a61
egor kolosov <e.k0losovkolosov@yandex.ru>
2022-12-02 04:33:44 +0300

    Конфликт решен

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def password ():
     s3 = ['1','2','3','4','5','6','7','8','9','0']
     result =''
     for i in range (11):
-        result += random.choice(s1)
+        result += random.choice(s1+s2+s3)
     return result
 
 
